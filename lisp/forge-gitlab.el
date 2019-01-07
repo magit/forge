693b78dc75025c429f55faad81eccccd0f3e7aca
Jonas Bernoulli <jonas@bernoul.li>
2019-01-07 20:55:03 +0100

    Fix major copy-paste error

--- lisp/forge-gitlab.el
+++ lisp/forge-gitlab.el
@@ -5,18 +5,18 @@
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Magit is free software; you can redistribute it and/or modify it
+;; Forge is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 ;;
-;; Magit is distributed in the hope that it will be useful, but WITHOUT
+;; Forge is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
 ;; License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with Magit.  If not, see http://www.gnu.org/licenses.
+;; along with Forge.  If not, see http://www.gnu.org/licenses.
 
 ;;; Code:
 
@@ -29,7 +29,7 @@
 ;;; Variables
 
 (defvar forge-gitlab-token-scopes '(api)
-  "The Gitlab API scopes needed by Magit.
+  "The Gitlab API scopes needed by Forge.
 
 `api' is the only required scope.  It gives read and write access
 to everything.  The Gitlab API provides more fine-grained scopes
--- lisp/forge.el
+++ lisp/forge.el
@@ -7,18 +7,18 @@
 ;; Homepage: https://github.com/magit/forge
 ;; Keywords: git tools vc
 
-;; Magit is free software; you can redistribute it and/or modify it
+;; Forge is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 ;;
-;; Magit is distributed in the hope that it will be useful, but WITHOUT
+;; Forge is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
 ;; License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with Magit.  If not, see http://www.gnu.org/licenses.
+;; along with Forge.  If not, see http://www.gnu.org/licenses.
 
 ;;; Commentary:
 
