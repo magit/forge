11e7cb9c6da50ec7a9754245a8af1743804de5a6
Jonas Bernoulli <jonas@bernoul.li>
2019-06-20 22:34:44 +0200

    No longer share listing buffers between repositories
    Re #150.

--- lisp/forge-list.el
+++ lisp/forge-list.el
@@ -88,7 +88,7 @@
 (defun forge-list-issues (repo)
   "List issues in a separate buffer."
   (interactive (list (forge-get-repository t)))
-  (forge--list-topics 'forge-issue-list-mode "*Issues*"
+  (forge--list-topics 'forge-issue-list-mode nil
     (forge-sql [:select $i1 :from issue :where (= repository $s2)]
                (forge--topic-list-columns-vector)
                (oref repo id))))
@@ -109,7 +109,7 @@
 (defun forge-list-pullreqs (repo)
   "List pull-requests in a separate buffer."
   (interactive (list (forge-get-repository t)))
-  (forge--list-topics 'forge-pullreq-list-mode "*Pull-Requests*"
+  (forge--list-topics 'forge-pullreq-list-mode nil
     (forge-sql [:select $i1 :from pullreq :where (= repository $s2)]
                (forge--topic-list-columns-vector)
                (oref repo id))))
@@ -129,7 +129,14 @@
 (defun forge--list-topics (mode buffer-name rows)
   (declare (indent 2))
   (let ((topdir (magit-toplevel)))
-    (with-current-buffer (get-buffer-create buffer-name)
+    (with-current-buffer
+        (get-buffer-create
+         (or buffer-name
+             (let ((repo (forge-get-repository t)))
+               (format "*%s: %s/%s*"
+                       (substring (symbol-name mode) 0 -5)
+                       (oref repo owner)
+                       (oref repo name)))))
       (setq default-directory topdir)
       (funcall mode)
       (setq tabulated-list-entries
