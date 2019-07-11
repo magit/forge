3a794c499e39142eac9c3094b7e241bfa7da74d5
Jonas Bernoulli <jonas@bernoul.li>
2019-07-11 17:14:50 +0200

    forge--url-regexp: Allow username and password in http url
    Closes #169.

--- lisp/forge-core.el
+++ lisp/forge-core.el
@@ -224,7 +224,8 @@ at that time."
     (user-error "Cannot browse non-forge remote %s" remote)))
 
 (defun forge--url-regexp ()
-  (concat "\\`\\(?:git://\\|[^/@]+@\\|ssh://\\(?:[^/@]+@\\)?\\|https?://\\)"
+  (concat "\\`\\(?:git://\\|[^/@]+@\\|ssh://\\(?:[^/@]+@\\)?"
+          "\\|https?://\\(?:[^/@]+@\\)?\\)"
           (regexp-opt (mapcar #'car forge-alist) t)
           "[:/]\\(.+?\\)"
           "\\(?:\\.git\\|/\\)?\\'"))
