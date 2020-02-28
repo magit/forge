455b0371689520b58fdccdaf9e7b1fff354f8b9b
Jonas Bernoulli <jonas@bernoul.li>
2020-02-28 13:10:28 +0100

    forge-fork: Support any Github and Gitlab host
    Closes #246.
    
    For other forges `forge--fork-repository' still has to be
    implemented.

--- lisp/forge-commands.el
+++ lisp/forge-commands.el
@@ -749,8 +749,7 @@ information."
 (defun forge-fork (fork remote)
   "Fork the current repository to FORK and add it as a REMOTE.
 If the fork already exists, then that isn't an error; the remote
-is added anyway.  Currently this only supports github.com and
-gitlab.com."
+is added anyway.  Currently this only supports Github and Gitlab."
   (interactive
    (let ((fork (magit-completing-read "Fork to"
                                       (mapcar #'car forge-owned-accounts))))
@@ -762,9 +761,8 @@ gitlab.com."
   (let ((repo (forge-get-repository 'stub)))
     (forge--fork-repository repo fork)
     (magit-remote-add remote
-                      (magit-clone--name-to-url
-                       (concat (substring (oref repo githost) nil -4) ":"
-                               fork "/" (oref repo name)))
+                      (magit-clone--format-url (oref repo githost) fork
+                                               (oref repo name))
                       (list "--fetch"))))
 
 ;;; Misc
