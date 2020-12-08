b4684a67e188ffe10c2bade7b12839ca7f510361
KARASZI István <ikaraszi@gmail.com>
2020-12-08 18:09:32 +0100

    Add a bitbucket implementation

--- lisp/forge-bitbucket.el
+++ lisp/forge-bitbucket.el
@@ -35,10 +35,18 @@
    (pullreq-post-url-format   :initform "https://%h/%o/%n/pull-requests/%i#comment-%I")
    (commit-url-format         :initform "https://%h/%o/%n/commits/%r")
    (branch-url-format         :initform "https://%h/%o/%n/branch/%r")
+   (file-url-format           :initform "https://%h/%o/%n/src/%r/%f#lines-%l")
    (remote-url-format         :initform "https://%h/%o/%n/src")
    (create-issue-url-format   :initform "https://%h/%o/%n/issues/new")
    (create-pullreq-url-format :initform "https://%h/%o/%n/pull-requests/new")))
 
 ;;; _
+
+(cl-defmethod forge--file-url ((repo forge-bitbucket-repository) rev file start end)
+  (let* ((start-fragment (number-to-string start))
+         (location-fragment (if end (concat start-fragment ":" (number-to-string end)) start-fragment)))
+    (forge--format repo 'file-url-format
+                   `((?r . ,rev) (?f . ,file) (?l . ,location-fragment)))))
+
 (provide 'forge-bitbucket)
 ;;; forge-bitbucket.el ends here
