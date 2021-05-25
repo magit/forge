551e51511e25505d14e05699a1707fd57e394a9a
Jonas Bernoulli <jonas@bernoul.li>
2021-05-25 15:45:08 +0200

    Release version 0.2.0

--- forge-pkg.el
+++ forge-pkg.el
@@ -1,17 +1,13 @@
 (define-package "forge" "0"
   "Access Git forges from Magit."
   '((emacs          "25.1")
-    (closql         "1.0.0")
-    (dash           "2.14.1")
+    (closql         "1.0.4")
+    (dash           "2.18.1")
     (emacsql-sqlite "3.0.0")
-    ;; This comes with important bug fixes and a workaround for
-    ;; an Emacs bug.
-    (ghub           "20190319")
-    (let-alist      "1.0.5")
-    ;; Forge currently isn't compatible with any releases Magit
-    ;; version.  When removing this kludge, then do not forget to
-    ;; also remove the related kludge from Forge's Melpa recipe.
-    (magit          "20190408")
-    (markdown-mode  "2.3")
-    (transient      "0.1.0"))
-  :url "https://github.com/magit/forge")
+    (ghub           "3.5.2")
+    (let-alist      "1.0.6")
+    (magit          "3.0.0")
+    (markdown-mode  "2.4")
+    (transient      "0.3.3"))
+  :homepage "https://github.com/magit/forge"
+  :keywords '("git" "tools" "vc"))
