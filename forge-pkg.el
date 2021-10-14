cdf34e7586a2d4edde7a6da38752741080b68233
Jonas Bernoulli <jonas@bernoul.li>
2021-10-14 19:07:26 +0200

    Release version 0.3.0

--- forge-pkg.el
+++ forge-pkg.el
@@ -1,14 +1,14 @@
 (define-package "forge" "0"
   "Access Git forges from Magit."
   '((emacs          "25.1")
-    (closql         "20210927")
-    (dash           "2.18.1")
+    (closql         "1.2.0")
+    (dash           "2.19.1")
     (emacsql-sqlite "3.0.0")
-    (ghub           "3.5.2")
+    (ghub           "3.5.4")
     (let-alist      "1.0.6")
-    (magit          "3.0.0")
+    (magit          "3.3.0")
     (markdown-mode  "2.4")
-    (transient      "0.3.3")
-    (yaml           "0.3.3"))
+    (transient      "0.3.6")
+    (yaml           "0.3.4"))
   :homepage "https://github.com/magit/forge"
   :keywords '("git" "tools" "vc"))
