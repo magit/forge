a754c419f70e3638ff2a0ae6d0b05d7220afe7b8
Jonas Bernoulli <jonas@bernoul.li>
2023-05-15 12:03:43 +0200

    Bump dependencies

--- forge-pkg.el
+++ forge-pkg.el
@@ -1,7 +1,7 @@
 (define-package "forge" "0.3.2.50-git"
   "Access Git forges from Magit."
   '((emacs          "25.1")
-    (compat         "29.1.3.4")
+    (compat         "29.1.4.1")
     (closql         "20230407")
     (dash           "2.19.1")
     (emacsql        "20230409")
@@ -9,7 +9,7 @@
     (let-alist      "1.0.6")
     (magit          "20230319")
     (markdown-mode  "2.4")
-    (transient      "0.3.6")
-    (yaml           "0.3.5"))
+    (transient      "0.4.0")
+    (yaml           "0.5.2"))
   :homepage "https://github.com/magit/forge"
   :keywords '("git" "tools" "vc"))
