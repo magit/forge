ecbcce94a5581077f8e1cda3e3f2a66c6dd52766
Jonas Bernoulli <jonas@bernoul.li>
2023-07-23 02:47:12 +0200

    forge--pull-notifications: Cosmetics

--- lisp/forge-github.el
+++ lisp/forge-github.el
@@ -367,7 +367,8 @@
                (forge--msg nil t nil "Storing notifications")
                (closql-with-transaction (forge-db)
                  (forge-sql [:delete-from notification
-                             :where (= forge $s1)] forge)
+                             :where (= forge $s1)]
+                            forge)
                  (pcase-dolist (`(,key ,repo ,query ,obj) notifs)
                    (closql-insert (forge-db) obj)
                    (forge--zap-repository-cache (forge-get-repository obj))
