01872e18bece5537beeac57cddfdbed2b05a4b82
Jonas Bernoulli <jonas@bernoul.li>
2023-10-04 01:18:46 +0200

    forge--insert-topic-contents: Use fallback width for topic numbers

--- lisp/forge-topic.el
+++ lisp/forge-topic.el
@@ -357,7 +357,8 @@ identifier."
 
 (cl-defmethod forge--insert-topic-contents ((topic forge-topic) width prefix)
   (with-slots (number title unread-p closed) topic
-    (insert (format (string-pad (forge--format-topic-id topic prefix) width)))
+    (insert (format (string-pad (forge--format-topic-id topic prefix)
+                                (or width 5))))
     (insert " ")
     (forge--insert-topic-marks topic)
     (insert " ")
