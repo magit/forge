44f105af324217e2ebd1b69feb5bc5e84b5441c6
Jonas Bernoulli <jonas@bernoul.li>
2023-10-06 17:59:43 +0200

    Move definitions of forge--repository-ids
    That also allows changing the section headings in "forge-repo.el".
    Other libraries will follow.

--- lisp/forge-repo.el
+++ lisp/forge-repo.el
@@ -84,51 +84,8 @@
 
 (defclass forge-noapi-repository (forge-repository) () :abstract t)
 
-;;; Core
-
-(cl-defmethod forge--repository-ids ((class (subclass forge-repository))
-                                     host owner name &optional stub)
-  "Return (OUR-ID . THEIR-ID) of the specified repository.
-If optional STUB is non-nil, then the IDs are not guaranteed to
-be unique.  Otherwise this method has to make an API request to
-retrieve THEIR-ID, the repository's ID on the forge.  In that
-case OUR-ID derives from THEIR-ID and is unique across all
-forges and hosts."
-  (pcase-let* ((`(,_githost ,apihost ,id ,_class)
-                (or (assoc host forge-alist)
-                    (error "No entry for %S in forge-alist" host)))
-               (path (format "%s/%s" owner name))
-               (their-id (and (not stub)
-                              (ghub-repository-id
-                               owner name
-                               :host apihost
-                               :auth 'forge
-                               :forge (forge--ghub-type-symbol class)))))
-    (cons (base64-encode-string
-           (format "%s:%s" id
-                   (cond (stub path)
-                         ((eq class 'forge-github-repository)
-                          ;; This is base64 encoded, according to
-                          ;; https://docs.github.com/en/graphql/reference/scalars#id.
-                          ;; Unfortunately that is not always true.
-                          ;; E.g., https://github.com/dit7ya/roamex.
-                          (condition-case nil
-                              (base64-decode-string their-id)
-                            (error their-id)))
-                         (t their-id)))
-           t)
-          (or their-id path))))
-
-(cl-defmethod forge--repository-ids ((_class (subclass forge-noapi-repository))
-                                     host owner name &optional _stub)
-  (let ((their-id (if owner (concat owner "/" name) name)))
-    (cons (base64-encode-string
-           (format "%s:%s"
-                   (nth 3 (or (assoc host forge-alist)
-                              (error "No entry for %S in forge-alist" host)))
-                   their-id)
-           t)
-          their-id)))
+;;; Query
+;;;; Get
 
 (defvar-local forge-buffer-repository nil)
 (put 'forge-buffer-repository 'permanent-local t)
@@ -257,7 +214,7 @@ Needed because Eieio does not support adding a method that takes
 no argument."
   (forge-get-repository nil))
 
-;;; Utilities
+;;;; Current
 
 (defun forge-current-repository (&optional demand)
   "Return the repository at point or being visited.
@@ -277,6 +234,54 @@ an error."
       (and (derived-mode-p 'forge-repository-list-mode)
            (forge-get-repository :id (tabulated-list-get-id)))
       (and demand (user-error "No repository at point"))))
+
+;;; Identity
+
+(cl-defmethod forge--repository-ids ((class (subclass forge-repository))
+                                     host owner name &optional stub)
+  "Return (OUR-ID . THEIR-ID) of the specified repository.
+If optional STUB is non-nil, then the IDs are not guaranteed to
+be unique.  Otherwise this method has to make an API request to
+retrieve THEIR-ID, the repository's ID on the forge.  In that
+case OUR-ID derives from THEIR-ID and is unique across all
+forges and hosts."
+  (pcase-let* ((`(,_githost ,apihost ,id ,_class)
+                (or (assoc host forge-alist)
+                    (error "No entry for %S in forge-alist" host)))
+               (path (format "%s/%s" owner name))
+               (their-id (and (not stub)
+                              (ghub-repository-id
+                               owner name
+                               :host apihost
+                               :auth 'forge
+                               :forge (forge--ghub-type-symbol class)))))
+    (cons (base64-encode-string
+           (format "%s:%s" id
+                   (cond (stub path)
+                         ((eq class 'forge-github-repository)
+                          ;; This is base64 encoded, according to
+                          ;; https://docs.github.com/en/graphql/reference/scalars#id.
+                          ;; Unfortunately that is not always true.
+                          ;; E.g., https://github.com/dit7ya/roamex.
+                          (condition-case nil
+                              (base64-decode-string their-id)
+                            (error their-id)))
+                         (t their-id)))
+           t)
+          (or their-id path))))
+
+(cl-defmethod forge--repository-ids ((_class (subclass forge-noapi-repository))
+                                     host owner name &optional _stub)
+  (let ((their-id (if owner (concat owner "/" name) name)))
+    (cons (base64-encode-string
+           (format "%s:%s"
+                   (nth 3 (or (assoc host forge-alist)
+                              (error "No entry for %S in forge-alist" host)))
+                   their-id)
+           t)
+          their-id)))
+
+;;; Read
 
 (defun forge-read-repository (prompt)
   (let ((choice (magit-completing-read
@@ -307,6 +312,8 @@ an error."
                  forge-alist)
      (mapcar #'car forge-alist))
    nil t))
+
+;;; Miscellaneous
 
 (defun forge--as-githost (host)
   (or (car (car (cl-member host forge-alist :test #'equal :key #'car)))
