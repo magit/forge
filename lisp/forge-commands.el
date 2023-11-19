b96edcb2dd150bd79fd635d5fa07b07806a50fc6
Jonas Bernoulli <jonas@bernoul.li>
2023-11-19 10:59:33 +0100

    forge-visit-this-repository: List all topics not just issues

--- lisp/forge-commands.el
+++ lisp/forge-commands.el
@@ -468,7 +468,7 @@ with a prefix argument also closed topics."
          (worktree (oref repo worktree)))
     (if (and worktree (file-directory-p worktree))
         (magit-status-setup-buffer worktree)
-      (forge-list-issues (oref repo id)))))
+      (forge-list-topics (oref repo id)))))
 
 ;;; Create
 
