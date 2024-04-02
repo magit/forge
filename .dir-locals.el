145e9fce35668178765f68bb218dd64cf0755e6a
Jonas Bernoulli <jonas@bernoul.li>
2024-04-02 19:13:59 +0200

    .dir-locals.el: Cosmetics

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ((emacs-lisp-mode
   (indent-tabs-mode . nil))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode))
  (makefile-gmake-mode
   (outline-regexp . "##"))
- (git-commit-mode
-  (git-commit-major-mode . git-commit-elisp-text-mode)))
+ )
