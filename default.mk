f5269196d106123638491aef9e2a20104ca64b28
Jonas Bernoulli <jonas@bernoul.li>
2024-08-17 18:46:44 +0200

    make: Re-generate %.texi if HEAD changed since previous run

--- default.mk
+++ default.mk
@@ -44,6 +44,7 @@ DOMAIN      ?= magit.vc
 CFRONT_DIST ?= E2LUHBKU1FBV02
 
 VERSION ?= $(shell test -e $(TOP).git && git describe --tags --abbrev=0 | cut -c2-)
+REVDESC := $(shell test -e $(TOP).git && git describe --tags)
 
 EMACS      ?= emacs
 EMACS_ARGS ?=
