33e240d360b8e3950e9c8b7024e3e763465c0c13
Jonas Bernoulli <jonas@bernoul.li>
2025-02-04 14:40:49 +0100

    Release version 0.4.7

--- lisp/forge.el
+++ lisp/forge.el
@@ -7,7 +7,7 @@
 ;; Homepage: https://github.com/magit/forge
 ;; Keywords: git tools vc
 
-;; Package-Version: 0.4.6
+;; Package-Version: 0.4.7
 ;; Package-Requires: (
 ;;     (emacs "29.1")
 ;;     (compat "30.0.2.0")
