fde32ee49fd86fa3097399ae3f7d814040d790c7
Jonas Bernoulli <jonas@bernoul.li>
2025-05-09 16:44:18 +0200

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,11 @@
-((emacs-lisp-mode
+((nil
   (indent-tabs-mode . nil))
+ (makefile-mode
+  (indent-tabs-mode . t))
  (git-commit-mode
   (git-commit-major-mode . git-commit-elisp-text-mode))
- (makefile-gmake-mode
-  (outline-regexp . "##"))
+ (".github/PULL_REQUEST_TEMPLATE"
+  (nil (truncate-lines . nil)))
  ("CHANGELOG"
-  (nil
-   (fill-column . 80)
-   (mode . display-fill-column-indicator)))
- )
+  (nil (fill-column . 70)
+       (mode . display-fill-column-indicator))))
