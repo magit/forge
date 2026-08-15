71e182e40186cc2a79fea298586905f26f48ae15
Jonas Bernoulli <jonas@bernoul.li>
2026-08-15 21:11:21 +0200

    .dir-locals.el: Extend lisp-indent-local-overrides

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,13 @@
 ((nil
   (indent-tabs-mode . nil))
  (emacs-lisp-mode
-  (lisp-indent-local-overrides . ((cond . 0) (interactive . 0))))
+  (lisp-indent-local-overrides
+   (cond . 0)
+   (cond-let--thread$ . defun)
+   (interactive . 0)
+   (make-obsolete-variable . 1)
+   (thread-first . defun)
+   (thread-last . defun)))
  (makefile-mode
   (indent-tabs-mode . t))
  (git-commit-mode
